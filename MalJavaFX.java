c75625b1f9de3a7406c57e0d1782aef44e2e78f4
Mrtacocat <Christoffer@riis.cc>
2022-03-10 15:55:59 +0100

    r

--- MalJavaFX.java
+++ MalJavaFX.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class javaFX extends Application {
+public class MalJavaFX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
