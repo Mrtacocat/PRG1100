26f510af199ef1230fd0cd3a77cf63ddfe1156b8
Mrtacocat <Christoffer@riis.cc>
2022-04-23 20:37:55 +0200

    exercies 1-3

---
+++ w3resource/Basic part-1/Exercise2.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+import static java.lang.Integer.*;
+
+public class Exercise2 {
+   
+    public static void main(String[] args) {
+        int x = 74;
+        int y = 36;
+        int r = x + y;
+        out.println(r);
+    }
+}
---
+++ w3resource/Basic part-1/Exercise3.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+import static java.lang.Integer.*;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 3;
+        int r = 50 / 3;
+        
+        out.println(r);
+    }
+}
